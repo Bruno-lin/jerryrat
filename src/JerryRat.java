f1a7c7f8feb63afdfbb2d0cf970d1f4c2f816fc2
Bruno <woshibruno@gmail.com>
2021-05-13 09:50:26 +0800

    GET

--- src/JerryRat.java
+++ src/JerryRat.java
@@ -1,13 +1,22 @@
+import util.ResponseHeaders;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Locale;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class JerryRat implements Runnable {
 
     public static final String SERVER_PORT = "8080";
     public static final String WEB_ROOT = "res/webroot";
     ServerSocket serverSocket;
+    ResponseHeaders responseHeaders;
+
 
     public JerryRat() throws IOException {
         serverSocket = new ServerSocket(Integer.parseInt(SERVER_PORT));
@@ -18,37 +27,86 @@ public class JerryRat implements Runnable {
         while (true) {
             try (
                     Socket clientSocket = serverSocket.accept();
-                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                    BufferedOutputStream out = new BufferedOutputStream(clientSocket.getOutputStream());
                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
             ) {
+                responseHeaders = new ResponseHeaders();
+
                 String request = in.readLine();
-                while (request != null) {
-                    String[] req = request.split(" ");
-                    String get = req[0].toLowerCase();
-                    if (!get.equals("get")) {
-                        break;
-                    }
-                    String requestPath = req[1];
-                    File requestFile = new File(WEB_ROOT + requestPath);
-                    if (requestFile.isDirectory()) {
-                        requestFile = new File(requestFile, "/index.html");
-                        FileReader fos = new FileReader(requestFile);
-                        char[] content = new char[(int) requestFile.length()];
-                        fos.read(content);
-                        out.println(String.valueOf(content));
-                    } else {
-                        FileReader fos = new FileReader(requestFile);
-                        char[] content = new char[(int) requestFile.length()];
-                        fos.read(content);
-                        out.println(String.valueOf(content));
-                    }
-                    request = in.readLine();
+                String[] requestParts = request.split(" ");
+
+                if (requestWrong(requestParts)) {
+                    responseHeaders.setStatusLine("400 Bad Request");
+                    responseHeaders.setDate(new Date());
+                    out.write(responseHeaders.toString().getBytes(StandardCharsets.UTF_8));
+                }
+
+                File file = getFile(requestParts[1]);
+                byte[] entityBody = getEntity(file);
+                responseHeaders.setDate(new Date());
+
+                out.write(responseHeaders.toString().getBytes(StandardCharsets.UTF_8));
+
+                if (entityBody != null) {
+                    out.write("\r\n\r\n".getBytes(StandardCharsets.UTF_8));
+                    out.write(entityBody);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
                 System.err.println("TCP连接错误！");
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
+    }
+
+    private File getFile(String requestPart) {
+        File file = new File(WEB_ROOT + requestPart);
+        if (!file.isFile()) {
+            file = new File(WEB_ROOT + "/index.html");
+        }
+        return file;
+    }
+
+    private byte[] getEntity(File file) throws Exception {
+        byte[] entityBody = Files.readAllBytes(file.toPath());
+        responseHeaders.setContentLength(String.valueOf(entityBody.length));
+        responseHeaders.setLastModified(new Date(file.lastModified()));
+        responseHeaders.setContentType(getContentType(file.getName()));
+        return entityBody;
+    }
+
+    private boolean requestWrong(String[] requestParts) {
+        return requestParts.length < 3 ||
+                !requestParts[0].equalsIgnoreCase("get") ||
+                !requestParts[2].equalsIgnoreCase("HTTP/1.0");
+    }
+
+    private String getContentType(String content) throws Exception {
+        Map<String, String> map = new HashMap<>();
+        File file = new File("res/webroot/mime.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        while (true) {
+            String attr = br.readLine();
+            if (attr == null) {
+                break;
+            }
+            String[] attrs = attr.split("\\s+");
+            for (int i = 0; i < attrs.length; i += 2) {
+                map.put(attrs[i], attrs[i + 1]);
+            }
+        }
+        return getType(content, map);
+    }
+
+    private String getType(String content, Map<String, String> map) {
+        String[] name = content.split("\\.");
+        String key = name[name.length - 1];
+        if (!map.containsKey(key)) {
+            return map.get(".*");
+        }
+        return map.get(key);
     }
 
     public static void main(String[] args) throws IOException {
---
+++ src/util/ResponseHeaders.java
@@ -0,0 +1,79 @@
+package util;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+
+public class ResponseHeaders {
+    //headers content
+    String statusLine;
+    String date;
+    String server;
+    String contentLength;
+    String contentType;
+    String lastModified;
+
+    //headers
+    List<String> headers;
+
+
+    public ResponseHeaders() {
+        headers = new ArrayList<>();
+        this.date = "Date: " + getDate(new Date());
+        this.server = "Server: JerryRat";
+        this.statusLine = "HTTP/1.0 200 OK";;
+    }
+
+    private void collectHeaders() {
+        headers.add(date);
+        headers.add(server);
+        headers.add(contentLength);
+        headers.add(contentType);
+        headers.add(lastModified);
+    }
+
+    public void setDate(Date date) {
+        this.date = "Date: " + getDate(date);
+    }
+
+    public void setServer(String server) {
+        this.server = "Server: " + server;
+    }
+
+    public void setStatusLine(String statusCode) {
+        this.statusLine = "HTTP/1.0 " + statusCode;
+    }
+
+    public void setContentLength(String contentLength) {
+        this.contentLength = "Content-Length: " + contentLength;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = "Content-Type: " + contentType;
+    }
+
+    public void setLastModified(Date date) {
+        this.lastModified = "Last-Modified: " + getDate(date);
+    }
+
+    String getDate(Date date) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz");
+        dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+        return dateFormat.format(date);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        collectHeaders();
+
+        for (String header : headers) {
+            if (header != null) {
+                builder.append(header).append("\r\n");
+            }
+        }
+        return builder.toString().trim();
+    }
+}
